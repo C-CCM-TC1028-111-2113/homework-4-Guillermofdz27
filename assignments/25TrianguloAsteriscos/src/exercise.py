dc29d93013792025a5a48c3347ff6a4643bec3ba
Guillermofdz27 <88850960+Guillermofdz27@users.noreply.github.com>
2021-09-05 20:13:29 -0500

    Update exercise.py

--- assignments/25TrianguloAsteriscos/src/exercise.py
+++ assignments/25TrianguloAsteriscos/src/exercise.py
@@ -1,6 +1,17 @@
 
 def main():
     #Escribe tu código debajo de esta línea
+    y= "*"
+    d= int(input("Enter triangle height: "))
+    x= d+1
+    w= x+ ((0-x)+1)
+
+    while x != w and x!= 0:
+        u= d-w
+        print(u*(" ")+y)
+        y= "*"+str(y)
+        w=w+1
+    
     pass
 
 
